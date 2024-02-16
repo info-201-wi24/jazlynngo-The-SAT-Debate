9d065860f12ba6753adaa8000a709ccf8a835b22
jazlynngo <jhngo@uw.edu>
2024-02-15 23:04:53 -0800

    rearragned code

--- Final Project - Data Wrangling.R
+++ Final Project - Data Wrangling.R
@@ -1,6 +1,7 @@
+library(dplyr)
+
 getwd()
 language <- read.csv("Children who speak a language other than English at home.csv")
 school_scores <- read.csv("school_scores 3.csv")
-library(dplyr)
 
 joined <- left_join(language, school_scores, by = "State.Name")
