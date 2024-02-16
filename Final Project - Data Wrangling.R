65459931cca12e40bcc3eb8d3a26e33287a16740
jazlynngo <jhngo@uw.edu>
2024-02-16 15:12:39 -0800

    deleted columns for cleaning data

--- Final Project - Data Wrangling.R
+++ Final Project - Data Wrangling.R
@@ -14,6 +14,15 @@ school_scores_and_lang_df <- left_join(language_df, school_scores_df, by = "Stat
 #clean joined df
 school_scores_and_lang_df <- school_scores_and_lang_df %>% 
                                 filter(State.Name != "Puerto Rico") %>% 
-                                filter(TimeFrame == Year)
+                                filter(TimeFrame == Year) %>% 
+                                select(-DataFormat, -LocationType, -Academic.Subjects.Arts.Music.Average.GPA,
+                                       -Academic.Subjects.Arts.Music.Average.Years, -Academic.Subjects.Mathematics.Average.GPA,
+                                       -Academic.Subjects.Mathematics.Average.Years, -Academic.Subjects.Natural.Sciences.Average.GPA,
+                                       -Academic.Subjects.Natural.Sciences.Average.Years, -Academic.Subjects.Social.Sciences.History.Average.GPA,
+                                       -Academic.Subjects.Social.Sciences.History.Average.Years, -GPA.A.minus.Verbal, -GPA.A.plus.Math, 
+                                       -GPA.A.plus.Test.takers, -GPA.A.plus.Verbal, -GPA.A.Math, -GPA.A.Test.takers, -GPA.A.Verbal, 
+                                       -GPA.B.Math)
+
+
 
 
