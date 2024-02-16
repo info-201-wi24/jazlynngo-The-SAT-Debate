bdc8b839dfbc8d5a41074d2999b4bcd64439ef40
jazlynngo <jhngo@uw.edu>
2024-02-16 14:35:33 -0800

    joined dataframes

--- Final Project - Data Wrangling.R
+++ Final Project - Data Wrangling.R
@@ -1,8 +1,15 @@
 library(dplyr)
+
+#DataFrames
 language_df <- read.csv("Children who speak a language other than English at home.csv")
 school_scores_df <- read.csv("school_scores 3.csv")
 
-language <- language_df %>% 
-              mutate(filter(DataFormat == "Number"))
-        
-joined <- left_join(language, school_scores, by = "State.Name")
+#cleaned language_df
+language_df <- language_df %>%
+                  filter(DataFormat == "Percent")
+
+#joined ddf
+school_scores_and_lang_df <- left_join(language_df, school_scores_df, by = "State.Name")
+
+
+
