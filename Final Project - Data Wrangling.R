cd52c3d10ead23b96f60e9bccfd6bee65e946701
jazlynngo <jhngo@uw.edu>
2024-02-16 16:10:20 -0800

    most of data wrangling

--- Final Project - Data Wrangling.R
+++ Final Project - Data Wrangling.R
@@ -4,25 +4,57 @@ library(dplyr)
 language_df <- read.csv("Children who speak a language other than English at home.csv")
 school_scores_df <- read.csv("school_scores 3.csv")
 
-#cleaned language_df
+#Joining DataFrames
+# cleaning language dataframe
 language_df <- language_df %>%
                   filter(DataFormat == "Percent")
 
-#joined df
+#joined dataframes
 school_scores_and_lang_df <- left_join(language_df, school_scores_df, by = "State.Name")
 
-#clean joined df
+#Cleaning Joined DataFrame
 school_scores_and_lang_df <- school_scores_and_lang_df %>% 
                                 filter(State.Name != "Puerto Rico") %>% 
-                                filter(TimeFrame == Year) %>% 
-                                select(-DataFormat, -LocationType, -Year, -Academic.Subjects.Arts.Music.Average.GPA,
-                                       -Academic.Subjects.Arts.Music.Average.Years, -Academic.Subjects.Mathematics.Average.GPA,
-                                       -Academic.Subjects.Mathematics.Average.Years, -Academic.Subjects.Natural.Sciences.Average.GPA,
-                                       -Academic.Subjects.Natural.Sciences.Average.Years, -Academic.Subjects.Social.Sciences.History.Average.GPA,
-                                       -Academic.Subjects.Social.Sciences.History.Average.Years, -GPA.A.minus.Verbal, -GPA.A.plus.Math, 
-                                       -GPA.A.plus.Test.takers, -GPA.A.plus.Verbal, -GPA.A.Math, -GPA.A.Test.takers, -GPA.A.Verbal, 
-                                       -GPA.B.Math)
+                                filter(TimeFrame == Year)
+                                # TO DO: select columns that we want 
+                                
+#NEW CATEGORICAL VARIABLE
+state_region_lookup <- data.frame(
+  State.Code = c("HI", "AK", "WA", "OR", "CA", "ID", "MT", "WY", "NV", "UT", "CO", "AZ", "NM", 
+                 "ND", "SD", "MN", "NE", "KS", "MO", "IA", "WI", "IL", "IN", "MI", "OH",
+                 "TX", "OK", "AR", "LA", "MS", "AL", "TN", "KY", "WV", "VA", "MD", "DE", "NC", "SC", "GA", "FL",
+                 "PA", "NJ", "NY", "CT", "MA", "RI", "NH", "VT", "ME"),
+  Region = c("West","West","West", "West", "West", "West", "West", "West", "West", "West", "West", "West", "West",
+             "Midwest", "Midwest", "Midwest", "Midwest", "Midwest", "Midwest", "Midwest", "Midwest", "Midwest", "Midwest", "Midwest", "Midwest",
+             "South", "South", "South", "South", "South", "South", "South", "South", "South", "South", "South", "South", "South", "South", "South", "South",
+             "Northeast", "Northeast", "Northeast", "Northeast", "Northeast", "Northeast", "Northeast", "Northeast", "Northeast"))
+
+school_scores_and_lang_df <- left_join(school_scores_and_lang_df, state_region_lookup, by = "State.Code")
+
+#NEW CONTINUOUS/NUMERICAL VARIABLE
+# setup
+#   average percent of children who speak another language
+national_second_language_avgs <- language_df %>%
+  filter(`State.Name` == "United States", DataFormat == "Percent")
+national_second_language_avgs <- national_second_language_avgs %>%
+  rename(`Extra Language Percent` = English.Speaker)
+
+#   join scores with national averages to get diff
+school_scores_and_lang_df <- school_scores_and_lang_df %>%
+  left_join(national_second_language_avgs, by = c("TimeFrame" = "TimeFrame"))
+
+# adding column for continuous numerical variable
+school_scores_and_lang_df <- school_scores_and_lang_df %>%
+  mutate((extra_lang_nat_avg_perc_diff = as.numeric(gsub("[^0-9.]", "", 
+  `English.Speaker`)) - as.numeric(gsub("[^0-9.]", "", `Extra Language Percent`))) * 100)
 
 
-
-
+# SUMMARY DATASET
+# Create a new dataset with averaged scores by state
+state_averages_df <- school_scores_and_lang_df %>%
+  group_by(`State.Code`) %>%
+  summarise(
+    Average_Math = mean(Total.Math, na.rm = TRUE),
+    Average_Verbal = mean(Total.Verbal, na.rm = TRUE),
+    Average_Extra_Lang_Perc = mean (as.numeric((English.Speaker), na.rm = TRUE) * 100)
+  )
